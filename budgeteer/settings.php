bbe3bc55f5134ec4363662eb090c33fe4c0fee8d
jvcimafranca <97377931+jvcimafranca@users.noreply.github.com>
2023-05-28 18:35:32 +0800

    Add files via upload

--- budgeteer/settings.php
+++ budgeteer/settings.php
@@ -66,42 +66,40 @@
 </head>
 <body>
 
-	<div class="navbar">
-			<div class="profile">
-				<img src="images/profile picture.png" alt="Profile Picture">
-				<p>John Doe</p>
-			</div>
-			<ul>
-				<li><a href="#">Profile</a></li>
-				<li>
-					<!--<a href="home.php" class="active">Home</a>-->
-					<?php
-					$userStartDate = date('Y-m-01');
-					$userEndDate = date('Y-m-t');
-							
-					echo '<a href="home.php?userStartDate='.$userStartDate.'&userEndDate='.$userEndDate.'">Home</a>';
-					?>
-				</li>
-				<li><a href="budget.php">Budget</a></li>
-				<li><a href="expense.php">Expense</a></li>
-				<li>
-					<!--<a href="balance.php">Statistics</a>-->
-					<?php
-					$userStartDate = date('Y-m-01');
-					$userEndDate = date('Y-m-t');
-							
-					echo '<a class="dropdown-item" href="balance.php?userStartDate='.$userStartDate.'&userEndDate='.$userEndDate.'">Statistics</a>';
-					?>
-				</li>
-				<li><a href="#">Notes</a></li>
-				<li><a href="calendar.php">Calendar</a></li>
-				<li><a href="settings.php" class="active">Settings</a></li>
-				<li><a href="logout.php">Log Out</a></li>
-			</ul>
-			<div class="logo">
-				<img src="images/Logo3.png" alt="Logo">
-			</div>
+<div class="navbar">
+		<div class="profile">
+
+			<img src="images/profile pic.png" alt="Profile Picture">
+			<p>John Doe</p>
 		</div>
+		<ul>
+			<li>
+        <?php
+          $userStartDate = date('Y-m-d');
+		      $userEndDate = date('Y-m-d');
+                  
+          echo '<a href="home.php?userStartDate='.$userStartDate.'&userEndDate='.$userEndDate.'">Home</a>';
+        ?>
+      </li>
+			<li><a href="budget.php">Budget</a></li>
+      <li><a href="expense.php">Expense</a></li>
+			<li>
+        <?php
+          $userStartDate = date('Y-m-01');
+          $userEndDate = date('Y-m-t');
+                  
+          echo '<a href="summary.php?userStartDate='.$userStartDate.'&userEndDate='.$userEndDate.'.&period=month">Statistics</a>';
+        ?>
+      </li>
+      <li><a href="#">Notes</a></li>
+      <li><a href="calendar.php">Calendar</a></li>
+      <li><a href="settings.php" class="active">Settings</a></li>
+      <li><a href="logout.php">Log Out</a></li>
+		</ul>
+        <div class="logo">
+            <img src="images/Logo3.png" alt="Logo">
+          </div>
+	</div>
 
 	<?php
        /*$servername = "localhost";
