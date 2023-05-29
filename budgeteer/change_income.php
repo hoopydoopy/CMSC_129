a89ca7807280e88d4d292fa3abe39e74c0b158c5
jvcimafranca <97377931+jvcimafranca@users.noreply.github.com>
2023-05-29 21:08:33 +0800

    Add files via upload

--- budgeteer/change_income.php
+++ budgeteer/change_income.php
@@ -30,39 +30,70 @@
 	<link rel="stylesheet" href="css/fontello.css">
 	-->
 
-	<link rel="stylesheet" href="homestyle.css">
+	<link rel="stylesheet" href="expensestyle.css">
 	<link href="https://fonts.googleapis.com/css2?family=Baloo+Paaji+2:wght@400;500;700&family=Fredoka+One&family=Roboto:wght@400;700;900&family=Varela+Round&display=swap" rel="stylesheet">
 
 </head>
 <body>
 
-	<div class="navbar">
-			<div class="profile">
-				<img src="images/profile picture.png" alt="Profile Picture">
-				<p>John Doe</p>
-			</div>
-			<ul>
-				<li><a href="#">Profile</a></li>
-				<li><a href="home.php">Home</a></li>
-				<li><a href="budget.php">Budget</a></li>
-				<li><a href="expense.php">Expense</a></li>
-				<li>
-					<!--<a href="balance.php">Statistics</a>-->
-					<?php
-					$userStartDate = date('Y-m-01');
-					$userEndDate = date('Y-m-t');
+<div class="navbar">
+		<div class="profile">
 
-					echo '<a class="dropdown-item" href="balance.php?userStartDate='.$userStartDate.'&userEndDate='.$userEndDate.'">Statistics</a>';
-					?>
-				</li>
-				<li><a href="#">Notes</a></li>
-				<li><a href="#">Calendar</a></li>
-				<li><a href="settings.php" class="active">Settings</a></li>
-			</ul>
-			<div class="logo">
-				<img src="images/Logo3.png" alt="Logo">
-			</div>
+			<img src="images/profile pic.png" alt="Profile Picture">
+			<?php
+				$servername = "localhost";
+				$username = "root";
+				$password = "";
+				$dbname = "my_budget";
+
+				$conn = new mysqli($servername, $username, $password, $dbname);
+
+				// Check connection
+				if (!$conn) {
+					die("Connection failed: " . mysqli_connect_error());
+				}
+
+				$user_id = $_SESSION['loggedUserId'];
+				$sql = "SELECT first_name FROM users WHERE user_id = '$user_id'";
+				$result =  mysqli_query($conn, $sql);
+				if (mysqli_num_rows($result) > 0) {
+					$row = mysqli_fetch_assoc($result);
+					$current_fname = $row['first_name'];
+				}
+
+				echo'<p>';
+				echo $current_fname;
+				echo'</p>';
+			?>
 		</div>
+		<ul>
+			<li>
+        <?php
+          $userStartDate = date('Y-m-d');
+		      $userEndDate = date('Y-m-d');
+
+          echo '<a href="home.php?userStartDate='.$userStartDate.'&userEndDate='.$userEndDate.'">Home</a>';
+        ?>
+      </li>
+			<li><a href="budget.php">Budget</a></li>
+      <li><a href="expense.php">Expense</a></li>
+			<li>
+        <?php
+          $userStartDate = date('Y-m-01');
+          $userEndDate = date('Y-m-t');
+
+          echo '<a href="summary.php?userStartDate='.$userStartDate.'&userEndDate='.$userEndDate.'.&period=month" class="active">Statistics</a>';
+        ?>
+      </li>
+      <li><a href="#">Notes</a></li>
+      <li><a href="calendar.php">Calendar</a></li>
+      <li><a href="settings.php">Settings</a></li>
+      <li><a href="logout.php">Log Out</a></li>
+		</ul>
+        <div class="logo">
+            <img src="images/Logo3.png" alt="Logo">
+          </div>
+	</div>
 
 	<?php
         $servername = "localhost";
@@ -120,6 +151,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 	?>
 	<!-- form to get income and comments -->
+
+	<div class="box">
+		<form class="budget-form" method="post">
+		<h3>EDIT INCOME</h3>
+		<div class="column">
+			<div class="amount-box">
+				<div class="amount"> <span class="">Income Amount</span> </div>
+				<input class="amountinput" type="text" name="expense_amount" value = "<?php echo $current_amount; ?>" required><br>
+			</div>
+		</div>
+
+		<div class="column">
+			<div class="amount-box">
+				<div class="amount"> <span class="">Comments</span> </div>
+				<input class="amountinput" type="text" name="expense_amount" value = "<?php echo $current_amount; ?>" required><br>
+			</div>
+			<br>
+			<br>
+			<input class="saveButton" type="submit" value="Save">
+		</div>
+
+
+		</form>
+	</div>
+
+</body>
+</html>
+<!--
 	<form method="post">
 		<label for="name">Income Amount:</label>
 		<input type="text" name="income_amount" value = "<?php echo $current_amount; ?>" required><br>
@@ -129,5 +188,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 		<input type="submit" value="Save Changes">
 	</form>
-</body>
-</html>
+-->
